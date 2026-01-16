6274df3882b214988bf947138bbf95b6df8ae6a9
Suanna Zhong <117539182+suanna0@users.noreply.github.com>
2026-01-16 08:05:50 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,135 +1,183 @@
-// Demonstration of drawing skeletons on poses with ml5 BlazePose.
-// Information: https://docs.ml5js.org/#/reference/bodypose
-// Requires: https://unpkg.com/ml5@1.3.0/dist/ml5.js & p5.js v.1.11.11
-// (via https://unpkg.com/ml5@1/dist/ml5.js)
-// Version of 10/22/2025. 
-// Learn more about the ml5.js project: https://ml5js.org/
-// License: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+// p5.js interface to Google MediaPipe Landmark Tracking
+// Combines face, hands, and bodies into one tracker.
+// See https://mediapipe-studio.webapps.google.com/home
+// Uses p5.js v.1.11.11 + MediaPipe v.0.10.22-rc.20250304
+// By Golan Levin, revised as of 10/21/2025
+//
+// This app demonstrates how to access:
+// - face points (e.g. clown nose)
+// - hand points (e.g. thumb plum)
+// - face metrics (e.g. jaw openness)
+// - body pose
 
-let video;
-let bodyPose;
-let poses = [];
-let connections;
-let bodyPoseOptions = { maxPoses: 1, flipHorizontal: true, runtime: "mediapipe"};
+//----------------------------------------------------
+// Don't change the names of these global variables.
+let handLandmarks;
+let poseLandmarks;
+let faceLandmarks;
+let myCapture;
 
-function preload() {
-  // Load the bodyPose model. See this page for options info:
-	// https://docs.ml5js.org/#/reference/bodypose?id=ml5bodypose
-  bodyPose = ml5.bodyPose("BlazePose", bodyPoseOptions);
+//----------------------------------------------------
+// For landmarks you want, set to true; set false the ones you don't.
+// You'll get best performance with just one or two sets of landmarks.
+// (Note, trackers set to false on startup can't be enabled later.)
+let trackingConfig = {
+  doAcquireHandLandmarks: true,
+  doAcquirePoseLandmarks: true,
+  doAcquireFaceLandmarks: true,
+  doAcquireFaceMetrics: true,
+  poseModelLiteOrFull: "full", /* "lite" (3MB) or "full" (6MB) */
+  cpuOrGpuString: "GPU", /* "GPU" or "CPU" */
+  maxNumHands: 2,
+  maxNumPoses: 1,
+  maxNumFaces: 1,
+};
+let checkboxHand;
+let checkboxFace;
+let checkboxPose;
+
+//------------------------------------------
+async function preload() {
+	preloadTracker();
 }
 
-//==================================================
+//------------------------------------------
 function setup() {
   createCanvas(640, 480);
-
-  // Create the video and hide it
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.hide();
-
-  // Start detecting poses in the webcam video
-  bodyPose.detectStart(video, gotPoses);
-  // Get the skeleton connection information
-  connections = bodyPose.getSkeleton();
-}
-
-//==================================================
-function draw() {
-	background('black'); 
-	drawWebcamVideo(); 
+	myCapture = createCapture(VIDEO);
+	myCapture.size(160,120); 
+  myCapture.hide();
+	initiateTracking(); 
 	
-	// Draw a clown nose. 
-	if (poses.length === 1){
-		let pose = poses[0];
-		let nosePoint = pose.keypoints[ML5BODY_NOSE];
-		fill('Maroon');
-    circle(nosePoint.x, nosePoint.y, 50);
+	checkboxHand = createCheckbox('hand', trackingConfig.doAcquireHandLandmarks);
+  checkboxHand.position(0, 20);
+	checkboxFace = createCheckbox('face', trackingConfig.doAcquireFaceLandmarks);
+  checkboxFace.position(0, 40);
+	checkboxPose = createCheckbox('pose', trackingConfig.doAcquirePoseLandmarks);
+  checkboxPose.position(0, 60);
+}
+
+
+
+
+//------------------------------------------
+function draw() {
+  background("white");
+  drawVideoBackground();
+	
+	trackingConfig.doAcquireHandLandmarks = checkboxHand.checked();
+	trackingConfig.doAcquireFaceLandmarks = checkboxFace.checked();
+	trackingConfig.doAcquirePoseLandmarks = checkboxPose.checked();
+	
+	// These functions are defined in trackerstuff.js
+  drawHandPoints();
+  drawPosePoints(); 
+  drawFacePoints();
+  drawFaceMetrics();
+	
+	// Example "applications"; for code, see below:
+	drawClownNose();
+	drawThumbPlum();
+	drawJawOpenness(); 
+	drawDiagnosticInfo();
+}
+
+//------------------------------------------
+function drawClownNose(){
+	// Study this to understand how to access face points.
+	// Here's the simplest possible mask, a clown nose.
+	if (trackingConfig.doAcquireFaceLandmarks) {
+    if (faceLandmarks && faceLandmarks.faceLandmarks) {
+      const nFaces = faceLandmarks.faceLandmarks.length;
+      if (nFaces > 0) {
+				for (let f=0; f<nFaces; f++){
+					
+					// Draw a red circle centered on the nose (point #1)
+					let aFace = faceLandmarks.faceLandmarks[f];
+					let noseIndex = 1;
+					let nosePt = aFace[noseIndex];
+					let nx = map(nosePt.x, 0,1, width,0);
+					let ny = map(nosePt.y, 0,1, 0,height);
+					fill(255,0,0, 180);
+					noStroke(); 
+					circle(nx,ny, 40); 
+					
+					// Draw a small circle whose index is based on mouseX
+					let mouseIndex = int(map(mouseX,0,width, 0,478)); 
+					mouseIndex = constrain(mouseIndex, 0,477);
+					let mousePt = aFace[mouseIndex];
+					let mx = map(mousePt.x, 0,1, width,0);
+					let my = map(mousePt.y, 0,1, 0,height);
+					text(mouseIndex, mx+6,my);
+					noFill();
+					stroke(0); 
+					circle(mx,my, 8);
+				}
+			}
+		}
 	}
-
-  // Draw the skeleton connections.
-  for (let i = 0; i < poses.length; i++) {
-    let pose = poses[i];
-    for (let j = 0; j < connections.length; j++) {
-      let pointAIndex = connections[j][0];
-      let pointBIndex = connections[j][1];
-      let pointA = pose.keypoints[pointAIndex];
-      let pointB = pose.keypoints[pointBIndex];
-      // Only draw a line if both points are confident enough
-      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
-        stroke(255, 0, 0);
-        strokeWeight(2);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
-      }
-    }
-  }
-
-  // Draw all the tracked landmark points
-  for (let i = 0; i < poses.length; i++) {
-    let pose = poses[i];
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      let keypoint = pose.keypoints[j];
-      // Only draw a circle if the keypoint's confidence is bigger than 0.1
-      if (keypoint.confidence > 0.1) {
-        fill(0, 255, 0);
-        noStroke();
-        circle(keypoint.x, keypoint.y, 10);
-      }
-    }
-  }
 }
 
-//==================================================
-// Callback function for when bodyPose outputs data
-function gotPoses(results) {
-  // Save the output to the poses variable
-  poses = results;
-}
-
-function drawWebcamVideo(){
-  // Draw the webcam video
-	push();
-	if (bodyPoseOptions.flipHorizontal){
-		translate(width,0); 
-		scale(-1,1);
+//------------------------------------------
+function drawThumbPlum(){
+	// Study this to understand how to access hand points.
+	// Stick in your thumb; pull out a plum. 
+	if (trackingConfig.doAcquireHandLandmarks) {
+    if (handLandmarks && handLandmarks.landmarks) {
+      const nHands = handLandmarks.landmarks.length;
+      if (nHands > 0) {
+				let joints = handLandmarks.landmarks[0];
+				let thumbPt = joints[THUMB_TIP];
+				let thx = map(thumbPt.x, 0,1, width,0);
+        let thy = map(thumbPt.y, 0,1, 0,height);
+				fill(128,0,128, 180);
+				noStroke(); 
+				circle(thx,thy, 40); 
+			}
+		}
 	}
-	let transparency = 100; // reduce this to make video transparent
-	tint(255,255,255,transparency); 
-  image(video, 0, 0, width, height);
-	pop();
 }
 
-// Point indices:
-// This information may be helpful:
-const ML5BODY_NOSE = 0;
-const ML5BODY_LEFT_EYE_INNER = 1;
-const ML5BODY_LEFT_EYE = 2;
-const ML5BODY_LEFT_EYE_OUTER = 3;
-const ML5BODY_RIGHT_EYE_INNER = 4;
-const ML5BODY_RIGHT_EYE = 5;
-const ML5BODY_RIGHT_EYE_OUTER = 6;
-const ML5BODY_LEFT_EAR = 7;
-const ML5BODY_RIGHT_EAR = 8;
-const ML5BODY_MOUTH_LEFT = 9;
-const ML5BODY_MOUTH_RIGHT = 10;
-const ML5BODY_LEFT_SHOULDER = 11;
-const ML5BODY_RIGHT_SHOULDER = 12;
-const ML5BODY_LEFT_ELBOW = 13;
-const ML5BODY_RIGHT_ELBOW = 14;
-const ML5BODY_LEFT_WRIST = 15;
-const ML5BODY_RIGHT_WRIST = 16;
-const ML5BODY_LEFT_PINKY = 17;
-const ML5BODY_RIGHT_PINKY = 18;
-const ML5BODY_LEFT_INDEX = 19;
-const ML5BODY_RIGHT_INDEX = 20;
-const ML5BODY_LEFT_THUMB = 21;
-const ML5BODY_RIGHT_THUMB = 22;
-const ML5BODY_LEFT_HIP = 23;
-const ML5BODY_RIGHT_HIP = 24;
-const ML5BODY_LEFT_KNEE = 25;
-const ML5BODY_RIGHT_KNEE = 26;
-const ML5BODY_LEFT_ANKLE = 27;
-const ML5BODY_RIGHT_ANKLE = 28;
-const ML5BODY_LEFT_HEEL = 29;
-const ML5BODY_RIGHT_HEEL = 30;
-const ML5BODY_LEFT_FOOT_INDEX = 31;
-const ML5BODY_RIGHT_FOOT_INDEX = 32;
+//------------------------------------------
+function drawJawOpenness(){
+	// Study this to understand how to access face metrics.
+	// In this case, we extract the "jaw openness" metric.
+  if (trackingConfig.doAcquireFaceLandmarks && 
+      trackingConfig.doAcquireFaceMetrics){
+    if (faceLandmarks && faceLandmarks.faceBlendshapes) {
+      const nFaces = faceLandmarks.faceLandmarks.length;
+			let aFaceMetrics = faceLandmarks.faceBlendshapes[0];
+			if (aFaceMetrics){
+				let which=25; // the "jawOpen" metric (0...1)
+				let jawOpenness01 = aFaceMetrics.categories[which].score;
+				let jawBarHeight = map(jawOpenness01,0,1, 0,height);
+				noStroke(); 
+				fill(0,0,0, 150);
+				rect(150,height, 40,0-jawBarHeight);
+				textSize(10);
+				text("jaw", 150, height-jawBarHeight-5);
+			}
+    }
+	}
+}
+
+//------------------------------------------
+function drawVideoBackground() {
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  tint(255, 255, 255, 72);
+  image(myCapture, 0, 0, width, height);
+  tint(255);
+  pop();
+}
+
+//------------------------------------------
+let frameRateAvg = 60.0; 
+function drawDiagnosticInfo() {
+  noStroke();
+  fill("black");
+  textSize(12); 
+	frameRateAvg = 0.98*frameRateAvg + 0.02*frameRate();
+  text("FPS: " + nf(frameRateAvg,1,2), 40, 30);
+}
